9639dbdb676aba7ebd1346de1c656a8220b66332
Aram Anderson <aram.j.anderson@gmail.com>
2017-08-01 10:50:57 -0600

    What can I make working

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -23,7 +23,9 @@ class Pantry
   end
 
   def convert_units_helper(value)
-    if value < 0.0999999
+    # if value.class == Float
+    #   value = other_helper(value)
+    if value < 1
       value = {:quantity => value * 1000, :units => "Milli-Units"}
     elsif value > 100
       value = {:quantity => value / 100, :units => "Centi-Units"}
@@ -32,9 +34,28 @@ class Pantry
     end
   end
 
+  def other_helper(value)
+    units_1 = (value % 1).round(value.to_s.length - 1)
+    units_2 = value.to_i
+
+    [convert_units_helper(units_1), convert_units_helper(units_2)]
+  end
+
   def add_to_cookbook(recipe)
     @cookbook << recipe
   end
+
+  def what_can_i_make
+    i_can_make = []
+    cookbook.each do |recipe|
+    recipe.ingredients.all? do |k, v|
+      if stock[k] > v
+      i_can_make << recipe.name
+      end
+    end
+  end
+  i_can_make.uniq
+end
 
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -56,6 +56,19 @@ class PantryTest < Minitest::Test
     assert_equal expected, pantry.convert_units(r)
   end
 
+  def test_it_can_convert_with_mixed_units
+    skip
+    expected = {"Cayenne Pepper" => [{quantity: 25, units: "Milli-Units"}, {quantity: 1, units: "Universal Units"}], "Cheese" => [{quantity: 75, units: "Universal Units"}], "Flour" => [{quantity: 5, units: "Centi-Units"}, {quantity: 50, units: "Universal Units"}]}
+
+    r = Recipe.new("Spicy Cheese Pizza")
+    r.add_ingredient("Cayenne Pepper", 1.025)
+    r.add_ingredient("Cheese", 75)
+    r.add_ingredient("Flour", 550)
+    pantry = Pantry.new
+    pantry.convert_units(r)
+
+  end
+
   def test_it_can_add_to_cookbook
     pantry = Pantry.new
 
@@ -73,14 +86,35 @@ class PantryTest < Minitest::Test
 
     assert_equal r1, pantry.cookbook.first
   end
-# # Stock some ingredients
-# pantry.restock("Cheese", 10)
-# pantry.restock("Flour", 20)
-# pantry.restock("Brine", 40)
-# pantry.restock("Raw nuts", 20)
-# pantry.restock("Salt", 20)
-# # What can i make?
-# pantry.what_can_i_make # => ["Brine Shot", "Peanuts"]
+
+  def test_it_can_find_what_to_make
+    pantry = Pantry.new
+
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+    r2 = Recipe.new("Brine Shot")
+    r2.add_ingredient("Brine", 10)
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    # Stock some ingredients
+    pantry.restock("Cheese", 10)
+    pantry.restock("Flour", 20)
+    pantry.restock("Brine", 40)
+    pantry.restock("Raw nuts", 20)
+    pantry.restock("Salt", 20)
+
+    assert_equal ["Brine Shot", "Peanuts"], pantry.what_can_i_make
+  end
+
+  def test_how_many_i_can_make
+  end
 # # How many can i make?
 # pantry.how_many_can_i_make # => {"Brine Shot" => 4, "Peanuts" => 2}
 
